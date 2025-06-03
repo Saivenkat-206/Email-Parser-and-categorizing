894ac3561935ed34a9abded421b0e447d776738f
R Saivenkat <158982097+Saivenkat-206@users.noreply.github.com>
2025-06-03 10:04:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import sqlite3
 
 conn = sqlite3.connect("emails.db")
 cur = conn.cursor()
-cur.execute("SELECT Category FROM emails")
+cur.execute("SELECT * FROM emails")
 rows = cur.fetchall()
 for row in rows:
-    print(row[0])
+    print(row)
     print("-" * 40)
